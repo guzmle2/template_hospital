f7ed92b5e7db692e5a68ecb25d191208648b5121
Leonor Guzman <ronoel54@gmail.com>
2017-02-06 16:41:06 -0430

    Site
    1er commit

---
+++ js/scriptWeb.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Leonor Guzman on 1/20/2016.
+ */
+(function($){
+    $(function(){
+        $('.button-collapse').sideNav();
+        $('.slider').slider({full_width: true});
+        $('.carousel').carousel();
+        // enable the visual refresh
+        google.maps.visualRefresh = true;
+
+        var map;
+        function initialize() {
+            var mapOptions = {
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            map = new google.maps.Map(document.getElementById('map-canvas'),
+                mapOptions);
+            // try HTML5 geolocation
+            if(navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    var pos = new google.maps.LatLng(position.coords.latitude,
+                        position.coords.longitude);
+
+                    var infowindow = new google.maps.InfoWindow({
+                        map: map,
+                        position: pos,
+                        content: 'Aqui es el sitio.'
+                    });
+
+                    map.setCenter(pos);
+                }, function() {
+                    handleNoGeolocation(true);
+                });
+            } else {
+                // browser doesn't support geolocation
+                handleNoGeolocation(false);
+            }
+        }
+
+        function handleNoGeolocation(errorFlag) {
+            if (errorFlag) {
+                var content = 'Error: The Geolocation service failed.';
+            } else {
+                var content = 'Error: Your browser doesn\'t support geolocation.';
+            }
+
+            var options = {
+                map: map,
+                position: new google.maps.LatLng(60, 105),
+                content: content
+            };
+
+            var infowindow = new google.maps.InfoWindow(options);
+            map.setCenter(options.position);
+        }
+        google.maps.event.addDomListener(window, 'load', initialize);
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
+
